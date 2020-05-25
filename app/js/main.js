6f90c7e7b549d7d1f835d42f4e4f83f9c26e439f
Le0Mag <lmag@yandex.ru>
2020-05-25 15:16:13 +0300

    begining release

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,20 @@
 $(function(){
 
   $(".rate-star").rateYo({
-    rating: 5,
-    starWidth: "12px",
-    readOnly: true
+    rating: 4.5,
+    starWidth: "16px",
+    spacing: "3px",
+    readOnly: true,
+    ratedFill: "#ffc000",
   });
 
-  // $('.product__slider').slick({
-  //   dots: true,
-  //   arrows: false,
-  //   slidesToShow: 4,
-  //   slidesToScroll: 4
-  // });
+  $('.product__wrapper').slick({
+    dots: false,
+    appendArrows: $('.product__btn'),
+    nextArrow: '<button type="submit" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="submit" class="slick-btn slick-prev"></button>'
+    
+  });
 
   // var mixer = mixitup('.products__inner-box');
 
